d735bba16ae51a1d617176969de7056d055418c1
Flavio S.V <124817700+Flavio-sv@users.noreply.github.com>
2025-10-31 01:54:11 -0300

    Add files via upload

---
+++ src/services/index.ts
@@ -0,0 +1,7 @@
+// Arquivo central para exportar todos os serviços
+export * from './api';
+export * from './types';
+export * from './auth.service';
+export * from './product.service';
+export * from './category.service';
+export * from './stock.service';
